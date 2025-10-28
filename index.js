fe310d41084cbc091e746ba19f952dc7c5f5b891
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-10-28 21:47:26 +0530

    feat: configure dotenv for environment variables and update response messages to include port

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT
+
  const app = express()
 
  
 
 app.get('/',(req , res)=>{
-    res.send("chut mari ke")
+    res.send("chut mari ke",port)
 })
 
 
 
 
 app.listen(port,()=>{
-    console.log("chut mari ke")
+    console.log("chut mari ke",port)
 })
 
