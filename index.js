4c225591226dd7b3ef17782ad88ce39b022b3cce
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-10-29 23:48:21 +0530

    feat: add user authentication routes and middleware integration

--- index.js
+++ index.js
@@ -61,6 +61,34 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1); // Exit the application with a failure code
   });
 
+  import { Router } from "express";
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(
+    // 1st argument: Multer middleware for file handling
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage", // <-- Typo fixed
+            maxCount: 1
+        }
+    ]),
+    // 2nd argument (after the comma): The final controller to handle the logic
+    registerUser
+);
+ 
+router.route("/login").post(loginUser) 
+
+router.route("/logout").post( authMiddleware  ,logoutUser)
+
+export default router;
 
 
 
