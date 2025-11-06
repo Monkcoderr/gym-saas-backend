9cb5ad8461a0f28865f5fdfec8bccaea975269c3
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-11-06 18:31:22 +0530

    fix: correct bcrypt salt method usage in user model password hashing

--- models/user.model.js
+++ models/user.model.js
@@ -33,7 +33,7 @@ if (!this.isModified('password')){
     return next()
 }
 
-const salt = await bcrypt.salt(10);
+const salt = await bcrypt.genSalt(10);
 this.password = await bcrypt.hash(this.password,salt)
 next();
 
