5c712d5ecf6a2bbda30e73029100c534596fc480
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-11-06 18:06:53 +0530

    feat: add user registration endpoint with validation

--- index.js
+++ index.js
@@ -118,6 +118,35 @@ app.delete('/api/members/:id', expressAsyncHandler(async (req, res) => {
 }));
 
 
+app.post('/api/users/register',expressAsyncHandler(async(req,res)=>{
+
+  const { name , email, password } = req.body;
+  
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400); // Bad Request
+    throw new Error('User with this email already exists.');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data.');
+  }
+
+}))
+
+
 
 
 
