6b9839a8ddb114dd6c16bb3878ab523a7a828fc7
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2025-11-06 18:16:54 +0530

    fix: correct bcrypt salt method usage in password hashing

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcryptjs";
 
 
 
@@ -26,6 +26,19 @@ name: {
     timestamps: true,
   }
 );
+
+userSchema.pre('save',async function (next){
+
+if (!this.isModified('password')){
+    return next()
+}
+
+const salt = await bcrypt.salt(10);
+this.password = await bcrypt.hash(this.password,salt)
+next();
+
+})
+
 
 const User = mongoose.model('User',userSchema)
 
